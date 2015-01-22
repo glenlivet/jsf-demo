78f9e9f55a58f3b8caa31b9309ffe4cb63adf05f
Zhang, Shulai <kikyou1986@hotmail.com>
2015-01-22 09:14:16 +0800

    updating

--- src/main/webapp/resources/sungard-southwest/js/css_helper.js
+++ src/main/webapp/resources/sungard-southwest/js/css_helper.js
@@ -6,11 +6,21 @@
 /**
  * 标准的 搜索栏 按钮栏 表格 页面，拉升表格 填充整个页面
  */
-var initCommonTablePage = function(){
+var adjustCommonTablePage = function(){
 	var windowHeight = $(window).innerHeight();
 	var searchboxHeight = $('.ik-searcharea').outerHeight();
 	var menubarHeight = $('.ik-menubar').outerHeight();
 	var paginatorHeight = $('.ik-datatable .ui-paginator').outerHeight();
 	$('.ik-datatable .ui-datatable-tablewrapper').height(windowHeight - searchboxHeight - menubarHeight
-			- paginatorHeight - 20);
+			- paginatorHeight - 20)
+			.css('overflow', 'auto')
+			.css('overflow-x','auto');
+};
+
+/**
+ * 调整textarea在表单中的样式
+ */
+var adjustTextareaCssInGrid = function(){
+	$('.ik-textarea-colspan-3').parent().attr("colspan", 3);
+	$('.ik-textarea-colspan-3').css("width", "92.3%");
 };
