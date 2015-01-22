aa1263cf211ffdb7257020eabfbe41705d5dcf9e
Zhang, Shulai <Shulai.Zhang@sungard.com>
2015-01-22 09:36:13 +0800

    ff

---
+++ src/main/java/org/ikgroup/domain/EmployeeModel.java
@@ -0,0 +1,9 @@
+package org.ikgroup.domain;
+
+import org.primefaces.model.LazyDataModel;
+
+public class EmployeeModel extends LazyDataModel<Employee> {
+
+	private static final long serialVersionUID = -3717637856683020209L;
+
+}
